64e9f9c63854d14f1649de24c30f7c6bdceb189e
Qutiess <202758080+Qutiess@users.noreply.github.com>
2026-05-07 12:15:10 +0800

    fix:add g to end of sed command

--- files/scripts/patch-verify-provenance.sh
+++ files/scripts/patch-verify-provenance.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
-sed 's:ghcr.io/secureblue/:ghcr.io/qutiess/' /usr/libexec/secureblue/verify-provenance.sh
-sed 's:github.com/secureblue/secureblue:github.com/qutiess/qutiess-laptop' /usr/libexec/secureblue/verify-provenance.sh
+sed 's:ghcr.io/secureblue/:ghcr.io/qutiess:g' /usr/libexec/secureblue/verify-provenance.sh
+sed 's:github.com/secureblue/secureblue:github.com/qutiess/qutiess-laptop:g' /usr/libexec/secureblue/verify-provenance.sh
