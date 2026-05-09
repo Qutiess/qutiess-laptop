533e591b60c974b63fbcf537cce04cf51fb93e05
Qutiess <202758080+Qutiess@users.noreply.github.com>
2026-05-09 17:20:24 +0800

    fix:add -i flag to sed

--- files/scripts/patch-verify-provenance.sh
+++ files/scripts/patch-verify-provenance.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
-sed 's:ghcr.io/secureblue/:ghcr.io/qutiess:g' /usr/libexec/secureblue/verify-provenance.sh
-sed 's:github.com/secureblue/secureblue:github.com/qutiess/qutiess-laptop:g' /usr/libexec/secureblue/verify-provenance.sh
+sed -i 's:ghcr.io/secureblue/:ghcr.io/qutiess:g' /usr/libexec/secureblue/verify-provenance.sh
+sed -i 's:github.com/secureblue/secureblue:github.com/qutiess/qutiess-laptop:g' /usr/libexec/secureblue/verify-provenance.sh
